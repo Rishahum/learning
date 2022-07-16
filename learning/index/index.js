8023c591428aba7ee07069a19142dd674c81f43a
Risha Sinha <rishasinha07@gmail.com>
2022-07-16 09:11:49 +0530

    initial commit

---
+++ learning/index/index.js
@@ -0,0 +1,32 @@
+var a;
+a=10;
+console.log(a);
+console.log(typeof a);
+
+a=true;
+console.log(a)
+console.log(typeof a)
+ a=null;
+ console.log(a);
+ console.log(typeof a)
+  var str='a'
+  console.log(typeof str)
+  str="how r u"
+  console.log(str)
+  var str;
+  str='how r u doing i m fine'
+  console.log(str);
+  str='how r u doing\ni m fine'
+  console.log(str)
+  str='m thik hu m bhi thik hu'
+  console.log(str)
+  var num=4300
+  console.log(`half of ${num} is ${num/2}`)
+  var num=2020
+  console.log(num)
+  var num='risha'
+
+  let l=100;
+  console.log(l);
+//   let l='risha';
+//   console.log(l);
