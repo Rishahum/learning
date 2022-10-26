11f9d63d35333ccd18e5ed4d0427e34b439e1353
Risha Sinha <rishasinha07@gmail.com>
2022-10-26 21:34:13 +0530

    initial commit

--- React/todo/todo/src/Components/Todo.js
+++ React/todo/todo/src/Components/Todo.js
@@ -7,20 +7,64 @@ export default class todo extends Component {
     this.state={
         tasks: [{id:1, task: "Revise" },
             {id:2, task: "Learn"},
-        ]
-    }
+        ],
+        curTask:" ",
+    };
   }
+  handleChange=(e)=>{
+    this.setState({
+      curTask:e.target.value,
+    });
+  };
+  
+  handleSubmit = () => {
+    this.setState({
+      tasks: [
+        ...this.state.tasks,
+        { task: this.state.curTask, id: this.state.tasks.length + 1 },
+      ],
+      curTask: "",
+    });
+  };
+  handleDeleteTasks = (id) => {
+    // let narr = [];
+    let narr = this.state.tasks.filter((taskObj)=>{
+      return taskObj.id != id;
+    });
+    this.setState({
+      tasks: [...narr],
+    });
+  };
+  // handleSubmit=()=>{
+  //   this.setState({
+  //     tasks:[...this.state.tasks,
+  //       {
+  //       task:this.state.curTask, id:this.state.tasks.length + 1
+  //     }],
+  //     curTask:""
+  //   })
+    //  handlesubmit=(e)=>{
+    //   narr=this.state.filter((taskobj)=>{
+    //     return taskobj!=id;
+    //   })
+    //   this.setState({
+    //     tasks:[...narr],
+    //   })
+    //  }
+  //}
   render(){
     return (
         <div>
-        <input type="text"></input>
-        <button >Submit</button>
+        <input type="text"
+        value={this.state.curTask}
+        onChange={this.handleChange}></input>
+        <button onClick={this.handleSubmit}>Submit</button>
         {
             this.state.tasks.map(function (taskobj){
                 return(
-                    <li>
+                    <li key={taskobj.id}>
                 <p>{taskobj.task}</p>
-                <button >Submit</button>
+                <button onClick={()=>{this.handleDeleteTasks(taskobj.id)}}>Delete</button>
                 </li>
                 )
             })
