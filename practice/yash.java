8bbdbffdb6fe57d99fc0dc1c629961e8e77f59e0
Risha Sinha <rishasinha07@gmail.com>
2022-10-30 09:27:38 +0530

    name

---
+++ practice/yash.java
@@ -0,0 +1,11 @@
+import  java.util.Scanner;
+class main{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter maths number");
+        int a=sc.nextInt();
+        System.out.println("english marks");
+        int b=sc.nextInt();
+        int percentage=(((a+b)/200)*100)f
+    }
+}
