67ac2f7780d73689c470806c474badf62e17c4cb
Risha Sinha <rishasinha07@gmail.com>
2022-11-06 10:17:36 +0530

    movies

--- React/movies/movies/src/Components/Banner.js
+++ React/movies/movies/src/Components/Banner.js
@@ -1,16 +1,27 @@
 import React,{ Component } from 'react'
+import { movies } from './getMovies'
 export default class Banner extends Component{
     render(){ 
+      // let movie=movies.results[0];
+      let movie="";
         return(
             <>
-        <div class="card" >
-  <img src="..." class="card-img-top" alt="..."/>
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
+             {movie=="" ?(
+              <div className="banner spinner-border" role="status">
+  <span class="visually-hidden">Loading...</span>
+</div>
+            ):(
+              <div className="card" >
+  <img src="..." className="card-img-top" alt="..."/>
+  <div className="card-body">
+    <h5 className="card-title">Card title</h5>
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" className="btn btn-primary">Go somewhere</a>
   </div>
 </div>
+)
+            }                     
+        
         </>
          
           
