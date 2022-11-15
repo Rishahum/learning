447295d95aac5a74caf8d5d486af1c3f89a5ac70
Risha Sinha <rishasinha07@gmail.com>
2022-11-15 06:03:36 +0530

    initial

--- React/movies/movies/src/Components/Banner.js
+++ React/movies/movies/src/Components/Banner.js
@@ -2,8 +2,8 @@ import React,{ Component } from 'react'
 import { movies } from './getMovies'
 export default class Banner extends Component{
     render(){ 
-      // let movie=movies.results[0];
-      let movie="";
+       let movie=movies.results[0];
+      // let movie="";
         return(
             <>
              {movie=="" ?(
@@ -11,11 +11,11 @@ export default class Banner extends Component{
   <span class="visually-hidden">Loading...</span>
 </div>
             ):(
-              <div className="card" >
-  <img src="..." className="card-img-top" alt="..."/>
+              <div className="card banner" >
+  <img src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`} className="card-img-top" alt="..."/>
   <div className="card-body">
-    <h5 className="card-title">Card title</h5>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <h5 className="card-title">{movie.original_title}</h5>
+    <p className="card-text">"Some quick example text to build on the card title and make up the bulk of the card's content" {movies.results[0].overview}.</p>
     <a href="#" className="btn btn-primary">Go somewhere</a>
   </div>
 </div>
